bdc5969ca3fe0b259535e0b52e9d3143d78fe0fc
Allen Zeng <suyuz@andrew.cmu.edu>
2016-01-27 16:51:40 -0500

    Git for the start of modal construction.

--- js/bg.js
+++ js/bg.js
@@ -1,6 +1,6 @@
 (function () {
 
-    var width, height, largeHeader, canvas, ctx, stars, target, animateHeader = true;
+    var width, height, largeHeader, canvas, ctx, enterprise, stars, target, img, pat, mousePos, animateHeader = true;
     var colors = [
         "193,69,164,", "0,172,230,", "121,253,250,"
     ];
@@ -24,15 +24,41 @@
         canvas.height = height;
         ctx = canvas.getContext('2d');
 
+        img = new Image();
+        img.src = "../img/letter.png";
+        stars = [];
+        canvasInit();
+        animate();
+    }
 
+    function getMousePos(canvas, evt) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+            x: evt.clientX - rect.left,
+            y: evt.clientY - rect.top
+        };
+    }
+
+    function writeMessage(canvas, message) {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.font = '18pt Calibri';
+        context.fillStyle = 'black';
+        context.fillText(message, 10, 25);
+    }
+
+    function canvasInit() {
+
+        enterprise = new Enterprise();
+        canvas.addEventListener('mousemove', function (evt) {
+            mousePos = getMousePos(canvas, evt);
+        }, false);
 
         // create stars
-        stars = [];
-        for (var x = 0; x < width * 0.2; x++) {
+
+        for (var x = 0; x < width * 0.01; x++) {
             var c = new Star();
             stars.push(c);
         }
-        animate();
     }
 
     // Event handling
@@ -56,20 +82,89 @@
 
     function animate() {
         if (animateHeader) {
+            if (enterprise.pos.x > width || enterprise.pos.y < 0 || enterprise.pos.y > height) {
+                CleanupCachedCanvasses();
+                canvasInit();
+            }
             ctx.clearRect(0, 0, width, height);
-            ctx.font = '80pt zag';
-            //            ctx.lineWidth = 0;
-            var img = new Image();
-            img.src = "img/bg.png";
-            var pat = ctx.createPattern(img, "repeat");
-            ctx.fillStyle = pat; //'#2dd3d3';
+            ctx.font = '80pt bold'; // curlz-mt';
+            pat = ctx.createPattern(img, "repeat-x");
+            ctx.fillStyle = pat;
             ctx.textAlign = "center";
             ctx.fillText('Allen Zeng', width / 2, height / 2);
+
+            enterprise.fly();
             for (var i in stars) {
                 stars[i].draws();
             }
+
         }
         requestAnimationFrame(animate);
+    }
+
+    function CleanupCachedCanvasses() {
+        var delList = new Array();
+        for (name in canvas) {
+            delList.push(name);
+        }
+        for (i = 0; i < delList.length; i++) {
+            delete canvas[delList[i]];
+        }
+        delete delList;
+    }
+
+    function Enterprise() {
+        var _this = this;
+
+        // constructor
+        (function () {
+            _this.pos = {};
+            init();
+        })();
+
+        function init() {
+            _this.image = new Image();
+            _this.image.src = "../img/enterprise-normal.png";
+            _this.reverse = new Image();
+            _this.reverse.src = "../img/enterprise-reverse.png";
+            _this.pos.x = 0; // - 10 * _this.image.width;
+            _this.pos.y = height * Math.random();
+            _this.xvelocity = 1.0 + Math.random() * 0.6;
+            _this.yvelocity = 1.0 - Math.random() * 2.0;
+            _this.widthRatio = 0.16;
+            _this.heightRatio = 0.16;
+        }
+
+        _this.fly = function () {
+            if (mousePos && mousePos.x && mousePos.x != _this.pos.x + _this.image.width * _this.widthRatio && mousePos.y != _this.pos.y + _this.image.height * _this.heightRatio / 2) {
+
+                _this.newyVelocity = (mousePos.y - _this.pos.y - _this.image.height * _this.heightRatio / 2) / 100;
+                ctx.save();
+                _this.newxVelocity = (mousePos.x - _this.pos.x - _this.image.width * _this.widthRatio) / 100;
+                _this.pos.x += _this.newxVelocity;
+                _this.pos.y += _this.newyVelocity;
+                ctx.translate(_this.pos.x + _this.image.width * _this.widthRatio, _this.pos.y + _this.image.height * _this.heightRatio / 2);
+                ctx.rotate(Math.atan(_this.newyVelocity / _this.newxVelocity));
+                ctx.translate(-(_this.pos.x + _this.image.width * _this.widthRatio), -(_this.pos.y + _this.image.height * _this.heightRatio / 2));
+
+                if (mousePos.x >= _this.pos.x + _this.image.width * _this.widthRatio) {
+                    ctx.drawImage(_this.image, _this.pos.x, _this.pos.y, _this.image.width * _this.widthRatio, _this.image.height * _this.heightRatio);
+                } else {
+                    ctx.drawImage(_this.reverse, _this.pos.x, _this.pos.y, _this.image.width * _this.widthRatio, _this.image.height * _this.heightRatio);
+                }
+            } else {
+                _this.pos.x += _this.xvelocity;
+                _this.pos.y += _this.yvelocity;
+                ctx.save();
+                ctx.translate(_this.pos.x, _this.pos.y);
+                ctx.rotate(Math.atan(_this.yvelocity / _this.xvelocity));
+                ctx.translate(-_this.pos.x, -_this.pos.y);
+
+                ctx.drawImage(_this.image, _this.pos.x, _this.pos.y, _this.image.width * _this.widthRatio, _this.image.height * _this.heightRatio);
+            }
+            ctx.restore();
+
+        }
     }
 
     // Canvas manipulation
@@ -87,7 +182,7 @@
             _this.pos.y = height + Math.random() * 300;
             _this.alpha = 0.1 + Math.random() * 0.3;
             _this.scale = 0.1 + Math.random() * 0.3;
-            _this.velocity = 0.2 + Math.random() * 0.6;
+            _this.velocity = 0.6 + Math.random() * 0.6;
             //            _this.velocity = 0.2 + (Math.abs(_this.pos.x - width / 5) / width) * Math.random() * 0.8;
             //            _this.r = 255 - Math.floor(Math.random() * 250);
             //            _this.g = 255 - Math.floor(Math.random() * 250);
@@ -99,8 +194,7 @@
             _this.rotation = 0.5 + Math.random() * 0.5;
             _this.limit = 60 + Math.random() * 100;
         }
-        //        console.log(_this.outerRadius + " " + _this.innerRadius);
-        //        console.log(_this.r + " " + _this.g + " " + _this.b);
+
         this.draw = function () {
             if (_this.alpha <= 0) {
                 init();
@@ -109,8 +203,6 @@
             _this.alpha -= 0.0005;
             ctx.beginPath();
             ctx.arc(_this.pos.x, _this.pos.y, _this.scale * 10, 0, 2 * Math.PI, false);
-
-            //            ctx.fillStyle = 'rgba(255,255,255,'+ _this.alpha+')';
             ctx.fillStyle = 'rgba(' + _this.r + ',' + _this.g + ',' + _this.b + ',' + _this.alpha + ')';
             ctx.fill();
         };
@@ -143,18 +235,11 @@
                 cy = _this.pos.y + Math.sin(rot) * _this.innerRadius;
                 ctx.lineTo(cx, cy);
                 rot += step;
-                //                console.log(i);
             }
-            //            _this.rot += Math.PI / 2 * 36;
-            //            ctx.rotate(Math.PI * 2 / 10);
             ctx.lineTo(_this.pos.x, _this.pos.y - _this.outerRadius);
-            //            ctx.fillStyle = 'rgba(255,255,255,' + _this.alpha + ')';
             ctx.fillStyle = 'rgba(' + colors[_this.color] + _this.alpha + ')';
-            //            ctx.fillStyle = 'rgba(' + _this.r + ',' + _this.g + ',' + _this.b + ',' + _this.alpha + ')';
             ctx.fill();
             ctx.restore();
-            // draw your object
-            //            ctx.closePath();
         }
     }
 
--- js/main.js
+++ js/main.js
@@ -13,25 +13,25 @@
 
     'use strict';
 
-//    var modal = document.getElementById('myModal');
-//
-//    // Get the button that opens the modal
-//    var btn = document.getElementById("myBtn");
-//
-//    // Get the <span> element that closes the modal
-//    var span = document.getElementsByClassName("close")[0];
-//
-//    // When the user clicks on <span> (x), close the modal
-//    span.onclick = function () {
-//        modal.style.display = "none";
-//    }
-//
-//    // When the user clicks anywhere outside of the modal, close it
-//    window.onclick = function (event) {
-//        if (event.target == modal) {
-//            modal.style.display = "none";
-//        }
-//    }
+    //    var modal = document.getElementById('myModal');
+    //
+    //    // Get the button that opens the modal
+    //    var btn = document.getElementById("myBtn");
+    //
+    //    // Get the <span> element that closes the modal
+    //    var span = document.getElementsByClassName("close")[0];
+    //
+    //    // When the user clicks on <span> (x), close the modal
+    //    span.onclick = function () {
+    //        modal.style.display = "none";
+    //    }
+    //
+    //    // When the user clicks anywhere outside of the modal, close it
+    //    window.onclick = function (event) {
+    //        if (event.target == modal) {
+    //            modal.style.display = "none";
+    //        }
+    //    }
 
     //    document.getElementById("effect-honey-twitter").addEventListener("click", function () {
     //        modal.style.display = "block";
@@ -43,7 +43,7 @@
             do {
                 curtop += obj.offsetTop;
             } while (obj = obj.offsetParent);
-            return [curtop];
+            return curtop;
         }
     }
 
@@ -56,13 +56,23 @@
     //            })
     //        }
     //    });
-    window.addEventListener("scroll", function () {
-        var height = window.innerHeight;
-        if (document.body.scrollTop > height - 10) {
-            document.getElementById("az-header").style.display = "block";
+    addEventListener("load", function () {
+        //        document.getElementById("az-header").style.display = "none";
+        //        var height = findPos(document.getElementById("az-tabs-section"));
+        //        if (document.getElementById("az-header").style.top !== height + "px") {
+        //            document.getElementById("az-header").style.top = height + "px";
+        //        }
+    });
+    addEventListener("scroll", function () {
+        var height = findPos(document.getElementById("az-tabs-section"));
+        if (document.body.scrollTop > height - 60) {
+            document.getElementById("az-header").style.position = "fixed";
+            document.getElementById("az-header").style.top = 0;
+            document.getElementsByClassName("glass")[0].style.backfaceVisibility = "false";
         } else {
-            document.getElementById("az-header").style.display = "none";
+            document.getElementById("az-header").style.position = "";
         }
+        //        console.log(document.getElementsByClassName("glass")[0].style);
     });
 
 })();
